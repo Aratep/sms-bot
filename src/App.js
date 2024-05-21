9e2fdc6a7e30213a60d8473d765025fb3e5ea951
aratep <tepanyan95@gmail.com>
2024-05-21 21:29:48 +0400

    reverted notify

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { useEffect } from "react";
 import { Routes, Route } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
-import { useDispatch, useSelector } from "react-redux";
+import { useDispatch } from "react-redux";
 
 // PAGES
 import MainPage from "pages/main/Main.page";
@@ -15,21 +15,15 @@ import SupportPage from "pages/support/Support.page";
 import TopUpPage from "pages/top-up/TopUp.page";
 // ACTIONS
 import { setTgHash } from "store/common/common.actions";
-// SLICES
-import { commonSelector } from "store/common/common.slice";
-// UTILS
-import { notify } from "./utils/helper-functions";
 // STYLES
 import "./App.scss";
 import "react-toastify/dist/ReactToastify.css";
 
 function App() {
   const dispatch = useDispatch();
-  const { tgHash } = useSelector(commonSelector);
 
   useEffect(() => {
     dispatch(setTgHash());
-    notify(tgHash);
   }, []);
 
   return (
